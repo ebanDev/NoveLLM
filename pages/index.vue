2a5307c0556e32cf21af4a64d47f52f465fce3c7
Eban <contact@eban.eu.org>
2024-05-22 17:07:52 +0200

    Fix project name in index

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ const novelsStore = useNovelsStore()
 <template>
   <div class="flex items-center justify-center w-full flex-col space-y-4">
     <h1 class="font-bold text-4xl text-center">
-      Welcome to LocalNoveLLM
+      Welcome to NoveLLM
     </h1>
     <UButton
         v-if="novelsStore.hfToken === ''"
